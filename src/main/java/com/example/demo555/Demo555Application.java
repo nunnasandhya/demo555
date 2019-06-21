c47b19645ac16f4c7b0292524f53863a70cb967e
nunnasandhya <nunnasandhya.777@gmail.com>
2019-06-21 15:53:51 +0530

    first commit

---
+++ src/main/java/com/example/demo555/Demo555Application.java
@@ -0,0 +1,13 @@
+package com.example.demo555;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo555Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo555Application.class, args);
+	}
+
+}
